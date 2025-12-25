45a4a0a825eea3bee5e33c9059e51b36f067bf19
Abhay <abhayhgarodi365@gmail.com>
2025-12-25 23:05:32 +0530

    initial commit

---
+++ Microservices example/Notification/src/main/java/com/example/Notification/Model/Notification.java
@@ -0,0 +1,11 @@
+package com.example.Notification.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Notification {
+    private int nid;
+    private String desc;
+}
