41be4a4d57b6445c6ed25bdbb105697fa1512ecc
Micro-Donation for student fees and medical bills <abhayhgarodi365@gmail.com>
2026-01-02 15:42:52 +0530

    initial commit

---
+++ Microservices example/User/src/main/java/com/example/User/Model/Post.java
@@ -0,0 +1,11 @@
+package com.example.User.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Post {
+    private int pid;
+    private String desc;
+}
